3e49b838bf30db73c1712c38a0eaac038c1c1d19
Jochen Weile <jochenweile@gmail.com>
2021-09-20 17:46:45 -0400

    more QC devel

--- src/barseq_qc.R
+++ src/barseq_qc.R
@@ -18,11 +18,108 @@
 
 options(stringsAsFactors=FALSE)
 
-#plot AllFreq vs Stdev
-with(lrs,plot(Uptake.F5.allfreq,Uptake.F5.sd,log="x",pch=20,col=yogitools::colAlpha(1,0.2)))
+library(argparser)
+
+p <- arg_parser(
+  "draw QC plots for barseq",
+  name="barseq_qc.R"
+)
+p <- add_argument(p, "lrs", help="lrs file")
+p <- add_argument(p, "counts", help="counts file")
+p <- add_argument(p, "outdir", help="output directory")
+pargs <- parse_args(p)
+
+dir.create(pargs$outdir,recursive=TRUE)
+
+lrs <- read.csv(pargs$lrs)
+counts <- read.csv(pargs$counts)
 
 
-hqLRs <- lrs[which(lrs$Uptake.F5.sd < 0.6 & lrs$Uptake.F5.allfreq > 1e-6),]
+# Plot count distribution ---------------------
+
+fcols <- grep("\\.allfreq$",colnames(lrs))
+assays <- sub("\\..+$","",colnames(lrs)[fcols])
+fcols <- fcols[!duplicated(assays)]
+
+minFreq <- unique(sort(unlist(lrs[,fcols])))[[2]]
+pseudoCount <- 10^(floor(log10(minFreq)))
+maxFreq <- max(lrs[,fcols])
+breaks <- seq(log10(pseudoCount),ceiling(log10(maxFreq)),0.05)
+
+pdf(paste0(pargs$outdir,"bcFreqDistr.pdf"),8.5,11)
+op <- par(mfrow=c(length(fcols),1),oma=c(2,2,2,2))
+for (fcol in fcols) {
+  hist(log10(lrs[,fcol]),
+    breaks=breaks,freq=FALSE,ylim=c(0,2),
+    col="steelblue3",border=NA,
+    xlab=expression(log[10](barcode~frequency)),
+    main=sub("\\..+$","",colnames(lrs)[[fcol]])
+  )
+  grid(NULL,NULL)
+}
+par(op)
+invisible(dev.off())
+
+
+
+# panelfun <- function(x,y,...) {
+#   points(x,y,...)
+#   grid(NULL,NULL)
+#   abline(v=0:1,h=0:1,col=c("firebrick3","chartreuse3"),lty="dashed")
+#   corr <- cor(yogitools::fin(cbind(x,y)))[1,2]
+#   text(0.5,0.5,sprintf("R=%.02f",corr),col="blue",cex=1.2)
+# }
+# with(varscores[["Uptake.F5"]],{
+#   scm <- score.multi
+#   scm[scm==score.single] <- NA
+#   pairs(
+#     cbind(score.single,scm,score.infer),
+#     labels=c("single-mutants","average over\nmulti-mutants","inverse\nmultiplicative\nmodel"),
+#     pch=".",col=yogitools::colAlpha(1,0.3),
+#     xlim=c(-.5,1.5),ylim=c(-.5,1.5),lower.panel=panelfun,upper.panel=panelfun
+#   )
+# })
+
+# bioreps <- with(scores[isSingleMut & scores$Uptake.F4.allfreq > 5e-7,],{
+#   do.call(rbind,tapply(1:length(hgvsp),hgvsp,function(is) {
+#     if (length(is)>1) {
+#       t(combn(Uptake.F5.score[is],2))
+#     } else NULL
+#   }))
+# })
+# plot(bioreps,
+#   xlab="replicate clone A score",ylab="replicate clone B score",
+#   pch=".",col=yogitools::colAlpha(1,0.3)
+# )
+# grid(NULL,NULL)
+# abline(v=0:1,h=0:1,col=c("firebrick3","chartreuse3"),lty="dashed")
+# corr <- cor(yogitools::fin(bioreps))[1,2]
+# text(0.5,0.5,sprintf("R=%.02f",corr),col="blue",cex=1.2)
+
+
+#plot AllFreq vs CV
+samples <- unique(sub("\\.allfreq","",colnames(lrs)[grep("allfreq$",colnames(lrs))]))
+pdf(paste0(pargs$outdir,"freqCV.pdf"),8.5,11)
+op <- par(mfrow=c(2,2),oma=c(2,2,2,2))
+for (sample in samples) {
+  fcol <- paste0(sample,".allfreq")
+  sdcol <- paste0(sample,".sd")
+  lrcol <- paste0(sample,".lr")
+  plot(
+    lrs[,fcol]+pseudoCount, lrs[,sdcol]/lrs[,lrcol],
+    xlab="read frequency 'all'",ylab="CV(LR)",
+    log="xy",pch=".",col=yogitools::colAlpha(1,0.2),
+    main=sample
+  )
+}
+par(op)
+invisible(dev.off())
+
+
+
+
+
+hqLRs <- lrs[which(lrs$Uptake.F5.allfreq > 1e-6),]
 
 isSyn <- sapply(strsplit(gsub("p\\.|\\[|\\]","",hqLRs$hgvsp),";"), function(muts) {
   all(grepl("=$",muts))
@@ -35,7 +132,7 @@ synLRs <- hqLRs[isSyn,]
 misLRs <- hqLRs[!(isSyn | hqLRs$codonChanges=="WT" | grepl("Ter",hqLRs$hgvsp) | grepl("fs",hqLRs$aaChanges)),]
 
 
-pdf("barseqDistributions_UptF5.pdf",7,6)
+pdf(paste0(pargs$outdir,"barseqDistributions_UptF5.pdf"),7,6)
 op <- par(mfcol=c(3,2))
 breaks <- seq(-5,5,0.05)
 hist(
@@ -69,7 +166,7 @@ hist(
 )
 abline(v=mean(yogitools::fin(misLRs$Uptake.F5.lr)),lty="dotted")
 par(op)
-dev.off()
+invisible(dev.off())
 
 
 stops <- unlist(sapply(strsplit(stopLRs[which(stopLRs$Surface.F4.lr > -0.5),"aaChanges"],"\\|"),function(muts){
