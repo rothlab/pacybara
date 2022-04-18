34aa04b36d4bfab6b8a8a1e88748df34c20f6649
Jochen Weile <jochenweile@gmail.com>
2022-04-18 11:19:02 -0400

    qc for pacybara

---
+++ src/pacybara_qc.R
@@ -0,0 +1,119 @@
+#!/usr/bin/env Rscript
+# Copyright (C) 2021  Jochen Weile, Roth Lab
+#
+# This file is part of BarseqPro.
+#
+# BarseqPro is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BarseqPro is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with BarseqPro.  If not, see <https://www.gnu.org/licenses/>.
+options(
+  stringsAsFactors=FALSE,
+  ignore.interactive=TRUE
+)
+
+library(argparser)
+library(yogitools)
+
+p <- arg_parser(
+  "pacybara QC",
+  name="pacybara_qc.R"
+)
+p <- add_argument(p, "clusters", help="translated clusters csv.gz file")
+p <- add_argument(p, "outdir", help="output directory")
+pargs <- parse_args(p)
+
+# pargs <- list(clusters="clusters_transl.csv.gz",outdir="qc/")
+
+if (!grepl("/$",pargs$outdir)) {
+  pargs$outdir <- paste0(pargs$outdir,"/")
+}
+dir.create(pargs$outdir,showWarnings=FALSE,recursive=TRUE)
+
+clusters <- read.csv(pargs$clusters)
+
+
+#######################################
+#analyze distribution of barcode sizes
+uptagDistro <- clusters$upBarcode|>table()|>table()
+sizeDistro <- clusters$size|>table()
+
+sizes <- 1:(uptagDistro|>names()|>as.integer()|>max())
+distros <- sizes|>as.character()|>sapply(\(x)c(up=uptagDistro[x],clust=sizeDistro[x]))
+distros[is.na(distros)] <- 0
+
+pdf(paste0(pargs$outdir,"clusterSizes.pdf"),5,5)
+distros|>barplot(beside=TRUE,border=NA,
+  xlab="size",ylab="count",
+  col=c("red","blue")
+)
+legend("topright",c("uptag counts","final cluster sizes"),fill=c("red","blue"))
+dev.off()
+
+#########################################
+# Filter for usable barcodes and CCS > 1
+cleanBarcodes <- with(clusters, upBarcode[upTagCollision==""] |> unique() )
+compromisedBarcodes <- with(clusters, upBarcode[upTagCollision=="collision"] |> unique() )
+
+pdf(paste0(pargs$outdir,"compromisedBC.pdf"),5,5)
+barplot(
+  c(unique=length(cleanBarcodes),compromised=length(compromisedBarcodes)),
+  border=NA,col=c("steelblue3","firebrick3"),ylab="#uptag barcodes"
+)
+dev.off()
+
+clusters <- clusters[with(clusters,upTagCollision=="" & size > 1),]
+
+##########################################
+# Draw coverage plot
+aaChanges <- strsplit(clusters$aaChanges[!grepl("-|fs|WT|silent|[A-Z*]{2,}",clusters$aaChanges)],"\\|")
+aaChangeTally <- table(do.call(c,aaChanges))
+aacs <- names(aaChangeTally)
+
+toAA <- substr(aacs,nchar(aacs),nchar(aacs))
+pos <- as.integer(substr(aacs,2,nchar(aacs)-1))
+aaScale <- yogitools::toChars("AVLIMFYWRHKDESTNQGCP*")
+y <- sapply(toAA,function(a)22-which(aaScale==a))
+colvals <- colorRampPalette(c("orange","firebrick3"))(max(aaChangeTally))[aaChangeTally]
+
+pdf(paste0(pargs$outdir,"pb_coverage.pdf"),60,5)
+plot(NA,type="n",xlim=c(0,max(pos)+1),ylim=c(0,22),
+  axes=FALSE,xlab="position",ylab="AA",xaxs="i"
+)
+axis(1)
+axis(2,21:1,aaScale)
+rect(.5,.5,max(pos)+.5,21.5,col="gray",border=NA)
+rect(pos-.5,y-.5,pos+.5,y+.5,col=colvals,border=NA)
+invisible(dev.off())
+
+
+#draw Census plot
+mutsPerClone <- c(
+  fs=sum(grepl("fs",clusters$aaChanges)),
+  if.indel=sum(grepl("-",clusters$aaChanges))+sum(grepl("\\d+[A-Z*]{2,}",clusters$aaChanges)),
+  WT=sum(grepl("WT|silent",clusters$aaChanges)),
+  table(sapply(aaChanges,length))
+)
+plotCols=c("firebrick3","orange","gray",rep("chartreuse3",length(mutsPerClone)-3))
+
+pdf(paste0(pargs$outdir,"/pb_census.pdf"),7,5)
+op <- par(las=3)
+barplot(
+  mutsPerClone,
+  beside=TRUE,col=plotCols,border=NA,
+  xlab="#variants in a given clone",
+  ylab="#clones",main=sprintf("%d useful clones out of %d passing filter",length(aaChanges),nrow(clusters))
+)
+par(op)
+invisible(dev.off())
+
+
+cat("Done!\n")
