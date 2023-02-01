ef219d2659a47f878401079247e7823b984d27ac
Jochen Weile <jochenweile@gmail.com>
2023-02-01 16:09:21 -0500

    more comments on merge script

--- src/pacybara_merge.R
+++ src/pacybara_merge.R
@@ -29,36 +29,50 @@ lib1 <- read.csv(pargs$lib1)
 lib2 <- read.csv(pargs$lib2)
 
 cat("Indexing...\n")
-#index the first library
+#index the first library by virtual barcode (to lookup row number)
 bcIdx1 <- with(lib1,tapply(1:nrow(lib1),lib1$virtualBarcode,c))
 bcIdx1 <- hash(names(bcIdx1),bcIdx1)
+#init a list of of pairs of rows (from lib2 and lib1) to be merged
+toMerge <- list()
+counter <- c(merge=0L,new=0L,conflict=0L)
 
 cat("Comparing...\n")
-toMerge <- list()
-
+#iterate over the rows of the second library
+#and record any entries that need to be added to the first library
 toAdd <- do.call(c,lapply(1:nrow(lib2), function(row) {
+	#check if the virtual barcode exists in the first library
 	vbc <- lib2[row,"virtualBarcode"]
 	hits <- bcIdx1[[vbc]]
 	if (is.null(hits)) {
+		#if not, record it in the toAdd list
+		counter[["new"]] <<- counter[["new"]]+1
 		return(row)
 	} else {
+		#if it already exists, check the genotype
 		if (lib2[row,"hgvsc"] %in% lib1[hits,"hgvsc"]) {
-			#then we're already covered
+			#if the genotypes match, then we're already covered
+			#record the pair of rows to be merged later
+		  counter[["merge"]] <<- counter[["merge"]]+1
 			mergeHit <- which(lib1[hits,"hgvsc"] == lib2[row,"hgvsc"])
 			toMerge[[length(toMerge)+1]] <<- c(row,hits[mergeHit])
 			return(NULL)
 		} else {
-			#it's a collision
+			#otherwise it's a collision and we add it as a separate entry to be 
+			#dealt with by the filter
+		  counter[["conflict"]] <<- counter[["conflict"]]+1
 			return(row)
 		}
 	} 
 }))
+
+print(counter)
 
 cat("Merging...\n")
 #make a copy of library one to modify for output
 out <- lib1
 
 #merge identical hits
+#they retain their barcodes and genos, but have their size and read list updated
 for (mergePair in toMerge) {
 	row <- mergePair[[1]]
 	hit <- mergePair[-1]
