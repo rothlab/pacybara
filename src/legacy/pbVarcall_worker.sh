cb068f4357548a2debdee5c8780f78f3436ea898
Jochen Weile <jochenweile@gmail.com>
2023-09-13 16:32:12 -0400

    bugfix: corrected insertion positions

--- src/legacy/pbVarcall_worker.sh
+++ src/legacy/pbVarcall_worker.sh
@@ -25,53 +25,57 @@ callVars() {
     ALNFILE=$1
     BARCODE=$2
     OUTFILE=$3
-    LLEN=60
-    # VARFILE=$(echo "$ALNFILE"|sed -r "s/\\.aln$/.vc/")
     #read all lines from the file into an array
     mapfile -t LINES<"$ALNFILE"
     #find the line number with FASTA headers (">")
-    HEADERS=($(grep -n ">" $1|cut -f 1 -d :))
-    printf "${BARCODE}\t">>$OUTFILE
+    #the outer parenthesis turns the result into an array
+    mapfile HEADERS<<<"$(grep -n ">" "$ALNFILE"|cut -f 1 -d :)"
+    printf "%s\t" "${BARCODE}">>"$OUTFILE"
     #the number of sequence alignment lines
-    NLINES=$((${HEADERS[1]}-2))
+    NLINES=$((HEADERS[1]-2))
+    #get the maximum line length (#characters in fasta line)
+    LLEN=${#LINES[2]}
+    #counter for number of insertions encountered
     INSCOUNT=0
+    #counter for total number of mutations encountered
     MCOUNT=0
     #i iterates over the lines in the alignment
-    for (( i = 0; i < $NLINES; i++ )); do
-        ALTLINE=${LINES[$(($i+1))]}
-        REFLINE=${LINES[$(($i+${HEADERS[1]}))]}
+    for (( i = 0; i < NLINES; i++ )); do
+        ALTLINE=${LINES[$((i+1))]}
+        REFLINE=${LINES[$((i+HEADERS[1]))]}
         # LLEN=${#REFLINE}
         # echo "$(($i+1)) $(($i+${HEADERS[1]})) - $LLEN"
         #j iterates over bases in line
-        for (( j = 0; j < $LLEN; j++ )); do
+        for (( j = 0; j < LLEN; j++ )); do
             #if the read differes from the reference at this base
-            if [[ ${ALTLINE:$j:1} != ${REFLINE:$j:1} ]]; then
+            if [[ "${ALTLINE:$j:1}" != "${REFLINE:$j:1}" ]]; then
                 #prepend a semicolon if this isn't the first mutation in here
-                if [[ $MCOUNT > 0 ]]; then
-                    printf ";">>$OUTFILE
+                if ((MCOUNT > 0)); then
+                    printf ";">>"$OUTFILE"
                 fi
                 #if the reference base is a "-", it's an insertion
                 if [[ ${REFLINE:$j:1} == "-" ]]; then
+                    POS=$((LLEN*i + j+1 - INSCOUNT))
                     ((INSCOUNT++))
-                    POS=$(($LLEN*$i + $j+1 - $INSCOUNT))
-                    printf "$(($POS-1))_${POS}ins${ALTLINE:$j:1}">>$OUTFILE
+                    printf "%d_%dins%s" "$((POS-1))" "${POS}" "${ALTLINE:$j:1}">>"$OUTFILE"
                 #if the read base is a "-", it's a deletion
                 elif [[ ${ALTLINE:$j:1} == "-" ]]; then
-                    POS=$(($LLEN*$i + $j+1 - $INSCOUNT))
-                    printf "${POS}del">>$OUTFILE
+                    POS=$((LLEN*i + j+1 - INSCOUNT))
+                    printf "%ddel" "${POS}">>"$OUTFILE"
                 #otherwise it's a substitution
                 else
-                    POS=$(($LLEN*$i + $j+1 - $INSCOUNT))
-                    printf "${POS}${REFLINE:$j:1}>${ALTLINE:$j:1}">>$OUTFILE
+                    POS=$((LLEN*i + j+1 - INSCOUNT))
+                    # printf "${POS}${REFLINE:$j:1}>${ALTLINE:$j:1}">>$OUTFILE
+                    printf "%d%s>%s" "${POS}" "${REFLINE:$j:1}" "${ALTLINE:$j:1}">>"$OUTFILE"
                 fi
                 ((MCOUNT++))
             fi
         done
     done
     if [[ $MCOUNT == 0 ]]; then
-        printf "=">>$OUTFILE
+        printf "=">>"$OUTFILE"
     fi
-    echo "">>$OUTFILE
+    echo "">>"$OUTFILE"
 }
 
 processChunk() {
@@ -87,10 +91,15 @@ processChunk() {
         BC=${SEQS[0]}
         # echo "Processing barcode: $BC"
         ALNOUT=alignments/${BC}.aln
-        echo "${SEQS[1]}"|needle -filter -bsequence $REFSEQ -outfile "$ALNOUT" -auto -aformat3 fasta &&\
+        #setting needle's gapopen penalty to -13 ensures POPs are not interpeted as double-indels
+        #default mismatch score is 5, mismatch is -4
+        # 3 mismatches = 3*(-4) = -12
+        # 2 gaps@10 = 2*(-10)+2*5 = -10
+        # 2 gaps@13 = 2*(-13)+2*5 = -16
+        echo "${SEQS[1]}"|needle -filter -bsequence "$REFSEQ" -gapopen 13.0 -outfile "$ALNOUT" -auto -aformat3 fasta &&\
         callVars "$ALNOUT" "$BC" "$OUTFILE" && rm "$ALNOUT"
     done < "$CHUNKFILE"
     rm "$CHUNKFILE"
 }
 
-processChunk $INFILE $REFSEQ
+processChunk "$INFILE" "$REFSEQ"
